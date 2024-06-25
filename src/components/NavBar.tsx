1e76c38d6150296b9e38537a56520426bd3c4be8
thopatevijay <thopatevijay@gmail.com>
2024-06-26 01:35:25 +0530

    wallet connected

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,8 +2,43 @@ import React from 'react';
 import Link from 'next/link';
 import { FaWallet } from 'react-icons/fa';
 import { FiHome, FiFilePlus } from 'react-icons/fi';
+import { useEffect, useState } from 'react'
+import { useConnectWallet } from '@web3-onboard/react';
+import { ethers } from 'ethers';
+interface Account {
+  address: string,
+}
+
 
 const NavBar = (): JSX.Element => {
+  const [{ wallet, connecting }, connect] = useConnectWallet()
+  const [ethersProvider, setProvider] = useState<ethers.providers.Web3Provider | null>()
+  const [account, setAccount] = useState<Account | null>(null)
+
+  useEffect(() => {
+    if (wallet?.provider) {
+      setAccount({
+        address: wallet.accounts[0].address,
+      })
+    }
+  }, [wallet])
+
+  useEffect(() => {
+    if (wallet?.provider) {
+      console.log("wallet", wallet)
+      setProvider(new ethers.providers.Web3Provider(wallet.provider, 'any'))
+    }
+  }, [connect, connecting, wallet])
+
+  const handleConnectClick = async () => {
+    try {
+      const walletStates = await connect();
+      console.log('Connected wallets:', walletStates);
+    } catch (error) {
+      console.error('Failed to connect wallet:', error);
+    }
+  };
+
   return (
     <nav className="bg-white shadow-md p-4 flex justify-between items-center">
       <div className="flex items-center space-x-4">
@@ -19,9 +54,12 @@ const NavBar = (): JSX.Element => {
           </Link>
         </div>
       </div>
-      <button className="flex items-center space-x-2 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
+      <button className="flex items-center space-x-2 bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700"
+        disabled={!!connecting || !!wallet?.provider}
+        onClick={handleConnectClick}
+      >
         <FaWallet />
-        <span>Connect Wallet</span>
+        <span>{account?.address ?? "Connect Wallet"}</span>
       </button>
     </nav>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,22 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from '../components/Layout';
+import { init, Web3OnboardProvider } from "@web3-onboard/react";
+import { onboardConfig } from "../utils/connectWallet";
+
+const wen3Onboard = init({
+  connect: {
+    autoConnectAllPreviousWallet: true,
+  },
+  ...onboardConfig,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Web3OnboardProvider web3Onboard={wen3Onboard}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Web3OnboardProvider>
   );
 }
