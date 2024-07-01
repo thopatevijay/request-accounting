48e591394124ed951a0f04bbce2c87eface809e4
thopatevijay <thopatevijay@gmail.com>
2024-07-01 18:32:37 +0530

    added Overview functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,8 +3,36 @@ import Head from 'next/head';
 import DashboardLayout from '../components/DashboardLayout';
 import Overview from '../components/Overview';
 import Notifications from '../components/Notifications';
+import { useRequestNetwork } from '../hooks/useRequestNetwork';
+import { useEffect, useState } from 'react';
+import { formatUnits } from "viem";
+import { getDecimals } from '@/utils';
 
 const Home: NextPage = () => {
+  const { fetchAllRequests, isLoading } = useRequestNetwork();
+  const [totalExpected, setTotalExpected] = useState(0);
+  const [totalReceived, setTotalReceived] = useState(0);
+  const [requestCount, setRequestCount] = useState(0);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const requests = await fetchAllRequests();
+      if (requests) {
+        setRequestCount(requests.length);
+        const totalExpectedAmount = requests.reduce((acc, request) => {
+          const decimals = getDecimals(request.currencyInfo.network!, request.currencyInfo.value);
+          const formattedAmount = parseFloat(formatUnits(BigInt(request.expectedAmount), decimals!));
+          return acc + formattedAmount;
+        }, 0);
+        const totalReceivedAmount = requests.reduce((acc, request) => acc + parseFloat(request.balance?.balance || '0'), 0);
+        setTotalExpected(totalExpectedAmount);
+        setTotalReceived(totalReceivedAmount);
+      }
+    };
+
+    fetchData();
+  }, [fetchAllRequests]);
+
   return (
     <DashboardLayout>
       <Head>
@@ -13,7 +41,12 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <Overview />
+      <Overview
+        totalExpected={totalExpected}
+        totalReceived={totalReceived}
+        requestCount={requestCount}
+        isLoading={isLoading}
+      />
       <Notifications />
     </DashboardLayout>
   );
